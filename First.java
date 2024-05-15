9b38134fc030d3568b00ba3c2a1954794d4a4a2c
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-05-15 16:54:44 +0530

    initial commit

--- First.java
+++ First.java
@@ -3,3 +3,10 @@ public class First {
         System.out.println("My First Java Program");
     }
 }
+
+/*
+Implementation of a Java application program involves the following steps.
+1. Creating the program
+2. Compiling the program
+3. Running the program
+*/
